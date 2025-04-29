40b856dae7750fe0bc29430b5429b37ef736c209
gface <89259112970@mail.ru>
2025-04-29 23:43:23 +0300

    Initial commit

---
+++ src/main/java/com/CardManagmentSystem/CardManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CardManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CardManagmentSystem/CardManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CardManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8e802a589fbd0fbbd6321e026a4e01c6a9cf8882
gface <89259112970@mail.ru>
2025-05-05 12:19:30 +0300

    add CardController, AuthController

---
+++ src/main/java/com/CardManagmentSystem/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.CardManagmentSystem.controller;
+
+import com.CardManagmentSystem.model.User;
+import com.CardManagmentSystem.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        String token = authService.login(user.getEmail(), user.getPassword());
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User registeredUser = authService.register(user);
+        return ResponseEntity.ok(registeredUser);
+    }
+}

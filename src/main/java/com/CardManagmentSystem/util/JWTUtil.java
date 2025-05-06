806c61d00f69a2d83f27d0bd0bd057691be81c06
gface <89259112970@mail.ru>
2025-05-06 13:50:51 +0300

    add JwtRequestFilter, UserDetailsServiceImpl

--- src/main/java/com/CardManagmentSystem/util/JWTUtil.java
+++ src/main/java/com/CardManagmentSystem/util/JWTUtil.java
@@ -1,5 +1,7 @@
 package com.CardManagmentSystem.util;
 
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -9,33 +11,48 @@ import java.util.Date;
 
 @Component
 public class JWTUtil {
-    private final String SECRET_KEY = "your_secret_key"; // Замените на свой секретный ключ
-    private final long EXPIRATION_TIME = 86400000; // 1 день
+
+    @Value("${jwt.secret}") // Секретный ключ из конфигурации
+    private String secretKey;
+
+    @Value("${jwt.expiration.time}") // Время истечения токена из конфигурации
+    private long expirationTime;
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
                 .compact();
     }
 
     public boolean validateToken(String token, String email) {
-        String tokenEmail = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-        return (tokenEmail.equals(email) && !isTokenExpired(token));
+        try {
+            String tokenEmail = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+            return (tokenEmail.equals(email) && !isTokenExpired(token));
+        } catch (Exception e) {
+            return false; // Возвращаем false в случае ошибки
+        }
     }
 
     private boolean isTokenExpired(String token) {
         Date expiration = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(secretKey)
                 .parseClaimsJws(token)
                 .getBody()
                 .getExpiration();
         return expiration.before(new Date());
+    }
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
     }
 }
